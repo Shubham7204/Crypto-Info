87758e6a5c76f43285851526b78a1285b7c07fde
Shubham7204 <83085067+Shubham7204@users.noreply.github.com>
2023-04-15 10:46:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+import CryptoNews from './Pages/CryptoNews';
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+        <Route path="/crypto-news" component={CryptoNews} exact />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
